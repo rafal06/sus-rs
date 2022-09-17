dbba06663d84fe7f0c57e1ac8c1fea57d825897f
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2022-09-17 18:39:12 +0200

    Add shortener API

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 #[macro_use] extern crate rocket;
+use serde::{Serialize, Deserialize};
+use rocket::serde::json::Json;
 
 mod shortener;
 use crate::shortener::gen_id;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Url {
+    url: String,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[post("/", data="<l_url>")]
+fn form_handler(l_url: Json<Url>) -> Json<Url> {
+    println!("{:?}", l_url.url);
+
+    Json( Url {
+        url: gen_id(),
+    })
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
+        .mount("/", routes![form_handler])
 }
