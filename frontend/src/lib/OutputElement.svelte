2841d0369a37b496744a73c50a6ed20c4512d406
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2022-10-17 23:02:50 +0200

    Refactor the expand button to transform rotate instead of 2 seperate imgs

--- frontend/src/lib/OutputElement.svelte
+++ frontend/src/lib/OutputElement.svelte
@@ -3,7 +3,6 @@
 
     import copyIcon from '/src/assets/copy.svg';
     import expandIcon from '/src/assets/expand.svg';
-    import collapseIcon from '/src/assets/collapse.svg';
 
     export let urlObj: Url;
 </script>
@@ -15,22 +14,12 @@
         <div class="icons">
             <img src={copyIcon} alt="copy" class="hover-shadow copy-btn" data-clipboard-target="#_{urlObj.id}">
 
-            <!-- If the long url is collapsed, show the expand icon -->
-            {#if !expandedUrls[urlObj.id]}
-                <img src={expandIcon}
-                    alt="expand"
-                    class="hover-shadow expand-btn"
-                    on:click={() => expandedUrls[urlObj.id] = !expandedUrls[urlObj.id]}
-                >
-            {:else}
-                <!-- Else, show the collapse icon -->
-                <img src={collapseIcon}
-                    alt="expand"
-                    class="hover-shadow expand-btn"
-                    on:click={() => expandedUrls[urlObj.id] = !expandedUrls[urlObj.id]}
-                >
-            {/if}
-            <!-- TODO: Instead of this ^ thing, just rotate the img -->
+            <img src={expandIcon}
+                alt="expand"
+                class="hover-shadow expand-btn"
+                class:expand-btn-collapsed={!!expandedUrls[urlObj.id]}
+                on:click={() => expandedUrls[urlObj.id] = !expandedUrls[urlObj.id]}
+            >
         </div>
     </div>
 
